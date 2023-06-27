5557517ac7deae9a6c05df8ca30b1c85da2b4192
Maycol R <maycol.rodriguez.ma@gmail.com>
2023-06-27 12:35:17 -0500

    pantalla de configuraciones terminada

--- app/src/main/java/com/rodriguez/weathermapsapp/ui/configuracion/ConfiguracionFragment.java
+++ app/src/main/java/com/rodriguez/weathermapsapp/ui/configuracion/ConfiguracionFragment.java
@@ -7,11 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.rodriguez.weathermapsapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ConfiguracionFragment extends Fragment {
+    Spinner fuente;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,18 @@ public class ConfiguracionFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view;
         view = inflater.inflate(R.layout.fragment_configuracion, container, false);
+        fuente = view.findViewById(R.id.spnFuente);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, getNumberList());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fuente.setAdapter(adapter);
         return view;
+    }
+
+    private List<Integer> getNumberList() {
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(12);
+        numberList.add(14);
+        numberList.add(16);
+        return numberList;
     }
 }
