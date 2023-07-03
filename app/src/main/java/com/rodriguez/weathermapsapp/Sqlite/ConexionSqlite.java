cc1405480e81e9d1d5bc4fd5eb2522c22a04ea3c
Maycol R <maycol.rodriguez.ma@gmail.com>
2023-07-03 00:35:37 -0500

    implementacion de sqlite

--- app/src/main/java/com/rodriguez/weathermapsapp/Sqlite/ConexionSqlite.java
+++ app/src/main/java/com/rodriguez/weathermapsapp/Sqlite/ConexionSqlite.java
@@ -1,2 +1,24 @@
-package com.rodriguez.weathermapsapp.Sqlite;public class ConexionSqlite {
+package com.rodriguez.weathermapsapp.Sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConexionSqlite extends SQLiteOpenHelper {
+    public ConexionSqlite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CreacionBd.CREAR_TABLA_FAVORITOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ CreacionBd.TABLA_FAVORITOS);
+        onCreate(db);
+    }
 }
